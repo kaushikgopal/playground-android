4e1b4d0818f9fee4be9abff5e6d89c7fa5e5114a
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-06 09:27:56 -0800

    upgrade kotlin-inject-anvil 0.1.0

--- app/src/main/java/sh/kau/playground/App.kt
+++ app/src/main/java/sh/kau/playground/App.kt
@@ -17,7 +17,11 @@ class App : Application() {
     // AndroidLogcatLogger.installOnDebuggableApp(this, minPriority = LogPriority.VERBOSE)
     CompositeLogger.install(appComponent.loggers)
 
+    // calling the field directly on appComponent
+    // is made possible because AppComponent inherits KotlinInjectAppComponentMerged directly
+    // you could alternatively provide another injection intermediate object
     logcat(LogPriority.INFO) { "xxx Welcome to ${appComponent.provideAppName()}" }
+
     logcat { "number of loggers: ${appComponent.loggers.size}" }
   }
 }
--- app/src/main/java/sh/kau/playground/di/AppComponent.kt
+++ app/src/main/java/sh/kau/playground/di/AppComponent.kt
@@ -3,7 +3,6 @@ package sh.kau.playground.di
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import logcat.LogcatLogger
-import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import sh.kau.domain.Named
 import sh.kau.playground.App
@@ -19,17 +18,16 @@ interface AppConfigComponent {
   @Provides fun provideAppName(): String = "My Playground!"
 
   @Provides
-  fun isDebuggableApp(app: App): @Named("debugApp") Boolean =
+  fun provideDebugApp(app: App): @Named("debugApp") Boolean =
       (app.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
 }
 
 // Merging
-@Component
 @MergeComponent(AppScope::class)
 @SingleIn(AppScope::class)
 abstract class AppComponent(
     @get:Provides val app: App,
-) : AppComponentMerged {
+) : KotlinInjectAppComponentMerged {
 
   abstract val loggers: Set<LogcatLogger>
 
