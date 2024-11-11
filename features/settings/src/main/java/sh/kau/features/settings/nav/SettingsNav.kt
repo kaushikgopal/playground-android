9ffc016890f04b6e6585c3242dbe8a42e48509d6
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-11 15:17:47 -0800

    nav subgraph implementation

--- features/settings/src/main/java/sh/kau/features/settings/nav/SettingsNav.kt
+++ features/settings/src/main/java/sh/kau/features/settings/nav/SettingsNav.kt
@@ -1,16 +1,30 @@
 package sh.kau.features.settings.nav
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import kotlinx.serialization.Serializable
-import sh.kau.features.settings.ui.SettingsScreen
+import sh.kau.features.settings.nav.SettingsRoutes.ScreenARoute
+import sh.kau.features.settings.nav.SettingsRoutes.ScreenBRoute
+import sh.kau.features.settings.nav.SettingsRoutes.SettingsGraphRoute
+import sh.kau.features.settings.ui.SettingsAScreen
+import sh.kau.features.settings.ui.SettingsBScreen
 
+sealed class SettingsRoutes {
+  @Serializable data object SettingsGraphRoute
 
-@Serializable
-object SettingsScreenRoute
+  @Serializable data object ScreenARoute
 
-fun NavGraphBuilder.settingsScreen() {
-    composable<SettingsScreenRoute> {
-        SettingsScreen()
-    }
+  @Serializable data object ScreenBRoute
+}
+
+fun NavGraphBuilder.buildSettingsGraph(
+    navGraphBuilder: NavGraphBuilder,
+    navHostController: NavHostController,
+) {
+  navGraphBuilder.navigation<SettingsGraphRoute>(startDestination = ScreenARoute) {
+    composable<ScreenARoute> { SettingsAScreen { navHostController.navigate(ScreenBRoute) } }
+    composable<ScreenBRoute> { SettingsBScreen() }
+  }
 }
