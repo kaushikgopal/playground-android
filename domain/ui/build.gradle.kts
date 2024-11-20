2c7d69876ac7a2885a9d7a1cc8885e9ce44ca60f
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-19 23:06:36 -0800

    remove template.feature plugin dep from :domain:ui

--- domain/ui/build.gradle.kts
+++ domain/ui/build.gradle.kts
@@ -1,8 +1,42 @@
 plugins {
     id("com.android.library")
-    id("template.feature")
+    alias(libs.plugins.kotlin.compose.compiler)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = libs.versions.app.namespace.get() + ".${project.parent?.name}.${project.name}"
+
+    compileSdk = libs.versions.sdk.compile.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.sdk.min.get().toInt()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    packaging {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+            excludes.add("/META-INF/LICENSE*")
+        }
+    }
+}
+
+dependencies {
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.compose.tooling.preview)
+    debugImplementation(libs.compose.tooling)
+
+    // Internal
+    implementation(project(":common:log"))
+    lintChecks(project(":common:lint-rules"))
 }
