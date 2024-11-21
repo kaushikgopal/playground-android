668b39e6f39eacc06c4ec0157d375f7c94ede4c9
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-20 17:20:06 -0800

    bolt | extract logger to LogComponent

--- app/src/main/java/sh/kau/playground/di/AppComponent.kt
+++ app/src/main/java/sh/kau/playground/di/AppComponent.kt
@@ -1,29 +1,17 @@
 package sh.kau.playground.di
 
 import android.content.Context
-import logcat.LogcatLogger
 import me.tatarka.inject.annotations.Component
-import me.tatarka.inject.annotations.IntoSet
-import me.tatarka.inject.annotations.Provides
-import sh.kau.playground.common.log.AndroidLogger
-import sh.kau.playground.common.log.AndroidLogger2
+import sh.kau.playground.common.log.di.LogComponent
 import sh.kau.playground.domain.shared.App
 import sh.kau.playground.domain.shared.di.ConfigComponent
 
 @Component
 abstract class AppComponent(
-    @Component val configComponent: ConfigComponent, // component inheritance
+    // component inheritance ↓
+    @Component val configComponent: ConfigComponent,
+    @Component val logComponent: LogComponent,
 ) {
-
-  @IntoSet
-  @Provides
-  protected fun provideAndroidLogger(logger: AndroidLogger): LogcatLogger = logger
-
-  @IntoSet
-  @Provides
-  protected fun provideAndroidLogger2(logger: AndroidLogger2): LogcatLogger = logger
-
-  abstract val loggers: Set<LogcatLogger> // multi-bindings
 
   companion object {
     private var instance: AppComponent? = null
@@ -32,9 +20,11 @@ abstract class AppComponent(
       if (instance != null) return instance!!
 
       val app = context.applicationContext as App
+      val config = ConfigComponent.create(app)
       instance =
           AppComponent::class.create(
-              ConfigComponent.create(app),
+              config,
+              LogComponent.create(config),
           )
 
       return instance!!
