dcef82506d3e737ccbfc669d63bba9f10a2d657d
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-22 14:34:18 -0800

    remove usage in composable

--- domain/ui/src/main/java/sh/kau/playground/domain/ui/UiUtils.kt
+++ domain/ui/src/main/java/sh/kau/playground/domain/ui/UiUtils.kt
@@ -4,18 +4,9 @@ import android.content.Context
 import sh.kau.playground.domain.shared.App
 
 /**
- * Usage:
- * ```kotlin
- * @Composable
- * fun MyComposable() {
- *     val app: App = LocalContext.current.app()
- *     // Use app instance...
- * }
- * ```
- *
  * This provides a type-safe way to access the Application instance from within a Composable,
  * typically used when trying to get a DI component setup.
  */
 fun Context.app(): App {
-    return applicationContext as App
+  return applicationContext as App
 }
