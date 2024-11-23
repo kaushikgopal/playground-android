0efde846f474a8e8731091e92e9cabb7881afcef
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-23 00:09:40 -0800

    bolt | settings quoter module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,23 +21,28 @@ rootProject.name = "Playground"
 // See [app-module-diagram.webp] for visual reference
 include(":app")
 
-// common modules (shared across the app) but swappable with other implementations
-include(
-    ":common:log", // android module
-    ":common:lint-rules",
-    ":common:networking",
-)
-
-// module specific to this app
-include(
-    ":domain:shared", // pure kotlin module |  @Named + ConfigComponent + referenced everywhere
-    ":domain:ui", // compose theme & design system
-    ":domain:app", // app level functionality
-    // features depend on this module preventing circular dependency over :app
-)
-
 // features of this app; each standalone
 include(
     ":features:landing", // typical feature
     ":features:settings",
 )
+
+// module specific to this app
+include(
+    ":domain:app", // app level functionality
+    // features depend on this module preventing circular dependency over :app
+
+    ":domain:ui", // compose theme & design system
+    ":domain:quoter", // produce quotes
+
+    ":domain:shared", // pure kotlin module |  @Named + ConfigComponent + referenced everywhere
+)
+
+
+// common modules (shared across the app) but swappable with other implementations
+include(
+    ":common:log", // android module
+    ":common:networking",
+    ":common:lint-rules",
+)
+
