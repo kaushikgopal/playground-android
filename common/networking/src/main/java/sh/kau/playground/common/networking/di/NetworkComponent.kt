62ea2c01b598ed276d84ca1143bd29430f6e1d7d
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-22 23:44:43 -0800

    bolt | network scope

--- common/networking/src/main/java/sh/kau/playground/common/networking/di/NetworkComponent.kt
+++ common/networking/src/main/java/sh/kau/playground/common/networking/di/NetworkComponent.kt
@@ -5,9 +5,10 @@ import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 import sh.kau.playground.common.networking.KtorClient
 
+@NetworkScope
 @Component
 abstract class NetworkComponent {
-  @Provides fun httpClient(): HttpClient = KtorClient
+  @NetworkScope @Provides fun httpClient(): HttpClient = KtorClient
 
   companion object {
     fun create() = NetworkComponent::class.create()
---
+++ common/networking/src/main/java/sh/kau/playground/common/networking/di/NetworkScope.kt
@@ -0,0 +1,11 @@
+package sh.kau.playground.common.networking.di
+
+import me.tatarka.inject.annotations.Scope
+
+@Scope
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+)
+annotation class NetworkScope
