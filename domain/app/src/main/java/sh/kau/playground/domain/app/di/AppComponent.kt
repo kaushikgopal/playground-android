fd00cd0a24c8fd93be80a66d346246a3280d5d97
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-24 18:14:10 -0800

    bolt | migrate AppComponent - @MergeComponent

--- domain/app/src/main/java/sh/kau/playground/domain/app/di/AppComponent.kt
+++ domain/app/src/main/java/sh/kau/playground/domain/app/di/AppComponent.kt
@@ -2,15 +2,18 @@ package sh.kau.playground.domain.app.di
 
 import android.content.Context
 import me.tatarka.inject.annotations.Component
+import me.tatarka.inject.annotations.Provides
 import sh.kau.playground.common.log.di.LogComponent
 import sh.kau.playground.domain.shared.App
 import sh.kau.playground.domain.shared.di.ConfigComponent
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
+import software.amazon.lastmile.kotlin.inject.anvil.MergeComponent
 import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 
+@MergeComponent(AppScope::class)
 @SingleIn(AppScope::class)
-@Component
 abstract class AppComponent(
+    @get:Provides val app: App,
     // component inheritance ↓
     // dependencies from below will now be available to AppComponent
     @Component val configComponent: ConfigComponent,
@@ -21,13 +24,14 @@ abstract class AppComponent(
     private var instance: AppComponent? = null
 
     fun from(context: Context): AppComponent {
-      val app = context.applicationContext as App
-
       if (instance != null) return instance!!
+
+      val app = context.applicationContext as App
 
       val config = ConfigComponent.Companion.create(app)
       instance =
           AppComponent::class.create(
+              context.applicationContext as App,
               config,
               LogComponent.Companion.create(config),
           )
