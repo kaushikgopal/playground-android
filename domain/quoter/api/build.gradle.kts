9ce857ffd4f7cc5be6022b088bdbe76cb644ae22
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-11-25 12:34:15 -0800

    revert domain:quoter:api android → jvm module

--- domain/quoter/api/build.gradle.kts
+++ domain/quoter/api/build.gradle.kts
@@ -1,8 +1,15 @@
+import org.gradle.kotlin.dsl.kotlin
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 dependencies {
+    // dependency injection
+    ksp(libs.bundles.kotlin.inject.compiler)
+    implementation(libs.bundles.kotlin.inject)
+
     implementation(libs.kotlinx.serialization.json)
 }
