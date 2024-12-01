5b22c84c26abe1c26297f5f929924782bd690dca
Kaushik Gopal <1042037+kaushikgopal@users.noreply.github.com>
2024-11-30 16:22:36 -0800

    comment for clarity in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 // adding it here => you don't have to manually add it in each subproject/build.gradle.kts
+//                   also avoids class loader issues when applied in other modules
+
 // apply false    => won't actually apply it to a project (you do that individually)
 plugins {
     alias(libs.plugins.android.application) apply false
