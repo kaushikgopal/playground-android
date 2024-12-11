f9ef47f3ad0a996e280e64ee6956cc076d25c825
kaush <1042037+kaushikgopal@users.noreply.github.com>
2024-12-10 20:35:49 -0800

    build improvement repository order
    https://developer.android.com/build/optimize-your-build#gradle_plugin_portal

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -16,9 +16,9 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     repositories {
+        google()
         mavenCentral()
         mavenLocal()
-        google()
     }
     versionCatalogs {
         create("libs") {
