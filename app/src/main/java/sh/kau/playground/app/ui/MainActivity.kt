fc55b94d648ef3fefc6ccdcc7403f1f01b2e0841
kaush <1042037+kaushikgopal@users.noreply.github.com>
2025-04-16 16:21:13 -0700

    move navhost to mainactivity

--- app/src/main/java/sh/kau/playground/app/ui/MainActivity.kt
+++ app/src/main/java/sh/kau/playground/app/ui/MainActivity.kt
@@ -5,32 +5,52 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import sh.kau.playground.app.AppNavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import sh.kau.playground.app.di.AppComponent
+import sh.kau.playground.features.landing.nav.LandingScreenRoute
+import sh.kau.playground.features.landing.nav.addLandingRoute
+import sh.kau.playground.features.settings.nav.SettingsRoutes.SettingsGraphRoute
+import sh.kau.playground.features.settings.nav.addSettingsGraph
 import sh.kau.playground.ui.AppTheme
 
 class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
 
     val appComponent = AppComponent.Companion.from(this)
 
+    enableEdgeToEdge()
     setContent {
       AppTheme {
+        val navController = rememberNavController()
+
         Scaffold(
             modifier = Modifier.Companion.fillMaxSize(),
         ) { innerPadding ->
+
           // important to pass and use the [innerPadding]
           // as adding top/bottom bar will be accounted for
           // in nested elements (as they will use the right padding value)
-          AppNavHost(
-              appComponent = appComponent,
-              innerPadding = innerPadding,
-          )
+
+          NavHost(
+              navController = navController,
+              startDestination = LandingScreenRoute, // starting screen
+          ) {
+            addLandingRoute(
+                modifier = Modifier.padding(innerPadding),
+                onNavigateToSettings = { navController.navigate(SettingsGraphRoute) },
+            )
+            addSettingsGraph(
+                settingsComponent = appComponent.createSettingsComponent(),
+                navGraphBuilder = this,
+                navHostController = navController,
+            )
+          }
         }
       }
     }
--- domain/ui/src/main/java/sh/kau/playground/ui/AppTheme.kt
+++ domain/ui/src/main/java/sh/kau/playground/ui/AppTheme.kt
@@ -15,6 +15,52 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true, // Dynamic color is available on Android 12+
+    content: @Composable () -> Unit
+) {
+  val context = LocalContext.current
+  val colorScheme =
+      when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+      }
+
+  val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val activity = view.context as Activity
+      if (canChangeSystemIconColor()) {
+        activity.window.statusBarColor = colorScheme.background.toArgb()
+
+        // 1. only the navigation bar requires the api check
+        //    but as a matter of taste i prefer to keep the colors standard
+        //    if i'm unable to change either of them
+        // 2. per material 3 design specs, the navigation bar has an elevation of 2
+        //    so you'd want to use a color overlay of 8% of the primary color
+        //    colorScheme.primary.copy(alpha =
+        // 0.08f).compositeOver(colorScheme.surface.copy()).toArgb()
+        //    but i prefer having the colors being identical
+        activity.window.navigationBarColor = colorScheme.background.toArgb()
+
+        // adjust the icons to match the colors chosen
+        WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars =
+            !darkTheme
+        WindowCompat.getInsetsController(activity.window, view).isAppearanceLightNavigationBars =
+            !darkTheme
+      }
+    }
+  }
+
+  MaterialTheme(colorScheme = colorScheme, typography = AppTypography, content = content)
+}
+
 private val DarkColorScheme =
     darkColorScheme(
         primary = Primary,
@@ -39,50 +85,5 @@ private val LightColorScheme =
         */
     )
 
-@Composable
-fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-  val colorScheme =
-      when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-      }
-
-  val view = LocalView.current
-  if (!view.isInEditMode) {
-    SideEffect {
-      val activity = view.context as Activity
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-        activity.window.statusBarColor = colorScheme.background.toArgb()
-
-        // 1. only the navigation bar requires the api check
-        //    but as a matter of taste i prefer to keep the colors standard
-        //    if i'm unable to change either of them
-        // 2. per material 3 design specs, the navigation bar has an elevation of 2
-        //    so you'd want to use a color overlay of 8% of the primary color
-        //    colorScheme.primary.copy(alpha =
-        // 0.08f).compositeOver(colorScheme.surface.copy()).toArgb()
-        //    but i prefer having the colors being identical
-        activity.window.navigationBarColor = colorScheme.background.toArgb()
-
-        // adjust the icons to match the colors chosen
-        WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars =
-            !darkTheme
-        WindowCompat.getInsetsController(activity.window, view).isAppearanceLightNavigationBars =
-            !darkTheme
-      }
-    }
-  }
-
-  MaterialTheme(colorScheme = colorScheme, typography = AppTypography, content = content)
-}
+private fun canChangeSystemIconColor(): Boolean =
+    Build.VERSION.SDK_INT >= 26 // Build.VERSION_CODES.O
